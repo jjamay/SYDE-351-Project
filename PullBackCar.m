06fc1d114b3de5057289e6934f487c016c06fcb3
Jordan May <jordaanmay@gmail.com>
2016-07-15 22:35:00 -0400

    simulation is somewhat working

---
+++ PullBackCar.m
@@ -0,0 +1,35 @@
+function dydt=PullBackCar(t,y, k1, m, R, b, k2, J)
+
+% STATE VECTOR
+%   y(:,1)  shaft momentum
+%   y(:,2)  armature flux
+
+% CONSTANT EXCITED
+% Constant excited parameters
+  dydt=[0 0 1/m 0; 
+      0 0 0 1/J; 
+      -k1 0 -b/(m*R^2) b/(R*J);
+      0 k2 b/(R*m) -b/J]*y;
+
+% SERIES EXCITED
+% Series excited parameters
+% SE11=220;R13=0.875;R33=1.75;beta=0.0307;L14=0.175;R23=0.066;J24=0.8;
+%  breaks=[0 1 1.1 2 2.1 3 3.1 4 4.1 20];coeffs=[1 1 .75 .75 .5 .5 .25 .25 0 0];
+
+% switch out current limiting resistances
+%  R13 = 3.5*interp1(breaks,coeffs,t)+ R13;
+%  dydt=[-(R23/J24)*y(1)+(beta/L14^2)*y(2)^2;y(2)*(-R13-R33)/L14-(beta/(L14*J24))*y(1)*y(2)+SE11];
+
+
+% PARALLEL EXCITED
+% Parallel excited parameters
+% SE11=220;R13=0.875;R33=1.75;beta=0.0307;L14=0.175;R23=0.066;J24=0.8;
+
+% switch in a torque load at 2.5s 
+ %if t>2.5
+  %   R23=R23*10;
+ %end
+
+ % dydt=[-(R23/J24)*y(1)+(beta*SE11)/(R33*L14)*y(2);y(2)*-R13/L14-(beta*SE11)/(R33*J24)*y(1)+SE11];
+
+end
---
+++ script.m
@@ -0,0 +1,19 @@
+k1 = 50;
+m = 10;
+R = 20;
+b = 10;
+k2 = 10;
+J = 10;
+
+options=odeset('RelTol',1e-4,'AbsTol',1e-8);
+[t,y]=ode45(@PullBackCar,[0 10],[10;0;0;0],options,k1,m,R,b,k2,J);
+
+% outpu10
+q1=y(:,1);
+%q8=y(:,2);
+%v2=y(:,3)/m;
+%w7=y(:,4)/J;
+plot(t,q1,'r'); 
+%plot(t,q8,'g');  
+%plot(t,v2,'b');
+%plot(t,w7,'r');
