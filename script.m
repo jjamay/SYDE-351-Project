5c08363ffde16c8631e0fc466ad80366179e3d11
Jordan May <jordaanmay@gmail.com>
2016-07-17 13:20:27 -0400

    All 4 state vars now have oscillating behaviour but are q1 and q8 are gradually increasing.

--- script.m
+++ script.m
@@ -1,19 +1,19 @@
 k1 = 10;
-m = 1;
+m = 0.5;
 R = 1;
-b = 100;
-k2 = 10;
-J = 10;
+b = 1000;
+k2 = 5;
+J = 1;
 
 options=odeset('RelTol',1e-4,'AbsTol',1e-8);
-[t,y]=ode45(@PullBackCar,[0 1],[0;0;0;0],options,k1,m,R,b,k2,J);
+[t,y]=ode45(@PullBackCar,[0 10],[0;10;0;0],options,k1,m,R,b,k2,J);
 
 % outpu10
 %q1=y(:,1);
-q8=y(:,2);
+%q8=y(:,2);
 %v2=y(:,3)/m;
 %w7=y(:,4)/J;
 %plot(t,q1,'r'); 
-plot(t,q8,'g');  
+%plot(t,q8,'g');  
 %plot(t,v2,'b');
 %plot(t,w7,'r');
