e1bc9d435b55d30a7af37c39d5cfd462518f4274
Justin Wong <justinwong12337@gmail.com>
2016-07-19 14:59:04 -0400

    Adding comments

--- script.m
+++ script.m
@@ -1,14 +1,20 @@
 % Real life parameters
 k1 = 37.817; % Translational K value
 k2 = 0.03083365392; % Rotational K value
-m = 0.1265; % Mass of the car
+m_car = 0.1265; % Mass of the car in kg
+m_wheel = 0.018; % Mass of wheel in kg
+outer_radius = 0.02; % Outer wheel radius in m (with tire)
+inner_radius = 0.01; % Inner wheel radius in m
 
-R = 1; 
-b = 100; 
-J = 1;
+% Moment of Inertia for the wheel - 0.5MR^2
+% ASSUMPTION: Wheel is modelled as hollow cylinder 
+J = 0.5*m_wheel*(outer_radius^2 + inner_radius^2);  
+
+% Unknown still 
+b = 100; % parameter for friction - what do we do with this?
 
 options=odeset('RelTol',1e-4,'AbsTol',1e-8);
-[t,y]=ode45(@PullBackCar,[0 100],[0;5;0;0],options,k1,m,R,b,k2,J);
+[t,y]=ode45(@PullBackCar,[0 10],[0;5;0;0],options,k1,m_car,outer_radius,b,k2,J);
 
 % output
 q1=y(:,1);
