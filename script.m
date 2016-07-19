9b151a0697f238b7934f8135f92508808a1526fd
Jordan May <jordaanmay@gmail.com>
2016-07-19 14:31:31 -0400

    played with values more.

--- script.m
+++ script.m
@@ -1,19 +1,19 @@
-k1 = 10;
-m = 0.5;
-R = 1;
-b = 1000;
-k2 = 5;
+k1 = 10; % Independent
+m = 1; % independent
+R = 1; % indepenent
+b = 100; % independent
+k2 = 1; % 
 J = 1;
 
 options=odeset('RelTol',1e-4,'AbsTol',1e-8);
-[t,y]=ode45(@PullBackCar,[0 10],[0;10;0;0],options,k1,m,R,b,k2,J);
+[t,y]=ode45(@PullBackCar,[0 100],[0;5;0;0],options,k1,m,R,b,k2,J);
 
-% outpu10
-%q1=y(:,1);
+% output
+q1=y(:,1);
 %q8=y(:,2);
 %v2=y(:,3)/m;
 %w7=y(:,4)/J;
-%plot(t,q1,'r'); 
+plot(t,q1,'r'); 
 %plot(t,q8,'g');  
 %plot(t,v2,'b');
 %plot(t,w7,'r');
